e8ea50fee65cbd6df0ca4cdb4b4fc1876e2c04c8
Naz Kin <nazkin@pop-os.localdomain>
2021-07-25 14:19:18 -0400

    binary search tree implemented along with its structure

--- app/binary_search_tree.py
+++ app/binary_search_tree.py
@@ -3,3 +3,27 @@ class Node:
         self.data=data
         self.left=None
         self.right=None
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert_recursively(self, value, node):
+        if value < node.data:
+            if node.left is None:
+                node.left = Node(value)
+            else:
+                self.insert_recursively(value, node.left)
+        elif value > node.data:
+            if node.right is None:
+                node.right = Node(value)
+            else:
+                self.insert_recursively(value, node.right)
+        else:
+            return
+
+    def insert_value(self, value):
+        if self.root is None:
+            self.root = Node(value)
+        else:
+            self.insert_recursively(value, self.root)
